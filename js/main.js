39d66c8e6bf5cd3cc87b1922ca94a8b8fcc1359b
MonstersBY <monsters.alex0@gmail.com>
2023-06-09 18:24:15 +0300

    feat: add client_portrait desc

--- js/main.js
+++ js/main.js
@@ -34614,6 +34614,481 @@ function createTags($box) {
 
 /***/ }),
 
+/***/ "./src/js/components/client_portrait__column.js":
+/*!******************************************************!*\
+  !*** ./src/js/components/client_portrait__column.js ***!
+  \******************************************************/
+/***/ (function() {
+
+if ($('#client_portrait__column-one').length) am5.ready(function () {
+  // Create root element
+  // https://www.amcharts.com/docs/v5/getting-started/#Root_element
+  var root = am5.Root["new"]("client_portrait__column-one");
+  root._logo.dispose();
+
+  // Set themes
+  // https://www.amcharts.com/docs/v5/concepts/themes/
+  root.setThemes([am5themes_Animated["new"](root)]);
+
+  // Create chart
+  // https://www.amcharts.com/docs/v5/charts/xy-chart/
+  var chart = root.container.children.push(am5xy.XYChart["new"](root, {
+    panX: false,
+    panY: false,
+    wheelX: "none",
+    wheelY: "none",
+    layout: root.verticalLayout
+  }));
+
+  // We don't want zoom-out button to appear while animating, so we hide it
+  chart.zoomOutButton.set("forceHidden", true);
+
+  // Create axes
+  // https://www.amcharts.com/docs/v5/charts/xy-chart/axes/
+  var yRenderer = am5xy.AxisRendererY["new"](root, {});
+
+  // yRenderer.grid.template.set("location", 1);
+  yRenderer.grid.template.set("forceHidden", true);
+  yRenderer.labels.template.set("forceHidden", true);
+  var yAxis = chart.yAxes.push(am5xy.CategoryAxis["new"](root, {
+    maxDeviation: 0,
+    categoryField: "product",
+    renderer: yRenderer,
+    tooltip: am5.Tooltip["new"](root, {
+      themeTags: ["axis"]
+    })
+  }));
+  var xAxis = chart.xAxes.push(am5xy.ValueAxis["new"](root, {
+    maxDeviation: 0,
+    min: 0,
+    extraMax: 0.1,
+    renderer: am5xy.AxisRendererX["new"](root, {
+      strokeOpacity: 0.1
+    })
+  }));
+
+  // Add series
+  // https://www.amcharts.com/docs/v5/charts/xy-chart/series/
+  var series = chart.series.push(am5xy.ColumnSeries["new"](root, {
+    name: "Series 1",
+    xAxis: xAxis,
+    yAxis: yAxis,
+    valueXField: "value",
+    categoryYField: "product",
+    tooltip: am5.Tooltip["new"](root, {
+      pointerOrientation: "left",
+      labelText: "{valueX}"
+    })
+  }));
+
+  // Rounded corners for columns
+  series.columns.template.setAll({
+    cornerRadiusTR: 5,
+    cornerRadiusBR: 5,
+    strokeOpacity: 0
+  });
+
+  // Make each column to be of a different color
+  series.columns.template.adapters.add("fill", function (fill, target) {
+    return chart.get("colors").getIndex(series.columns.indexOf(target));
+  });
+  series.columns.template.adapters.add("stroke", function (stroke, target) {
+    return chart.get("colors").getIndex(series.columns.indexOf(target));
+  });
+
+  // series.columns.template.adapters.add("fillGradient", am5.LinearGradient.new(root, {
+  // 	stops: [{
+  // 	  color: am5.color(0xffffff)
+  // 	}, {
+  // 	  color: am5.color(0x946b49)
+  // 	}],
+  // 	rotation: 0
+  // }));
+
+  // Set data
+  var data = [{
+    product: "Бады",
+    value: 2025
+  }, {
+    product: "Снижение сахара",
+    value: 1882
+  }, {
+    product: "Снеки",
+    value: 1809
+  }, {
+    product: "Корма для животных",
+    value: 1322
+  }, {
+    product: "Газированные напитки",
+    value: 1122
+  }, {
+    product: "Энергетические напитки",
+    value: 1114
+  }, {
+    product: "Безалкогольные напитки",
+    value: 984
+  }];
+  series.bullets.push(function () {
+    return am5.Bullet["new"](root, {
+      locationX: 1,
+      sprite: am5.Label["new"](root, {
+        text: "{product}",
+        fill: root.interfaceColors.get("alternativeText"),
+        centerX: am5.p100,
+        centerY: am5.p50,
+        populateText: true
+      })
+    });
+  });
+  yAxis.data.setAll(data);
+  series.data.setAll(data);
+
+  // Make stuff animate on load
+  // https://www.amcharts.com/docs/v5/concepts/animations/
+  series.appear(1000);
+  chart.appear(1000, 100);
+});
+if ($('#client_portrait__column-two').length) am5.ready(function () {
+  // Create root element
+  // https://www.amcharts.com/docs/v5/getting-started/#Root_element
+  var root = am5.Root["new"]("client_portrait__column-two");
+  root._logo.dispose();
+
+  // Set themes
+  // https://www.amcharts.com/docs/v5/concepts/themes/
+  root.setThemes([am5themes_Animated["new"](root)]);
+
+  // Create chart
+  // https://www.amcharts.com/docs/v5/charts/xy-chart/
+  var chart = root.container.children.push(am5xy.XYChart["new"](root, {
+    panX: false,
+    panY: false,
+    wheelX: "none",
+    wheelY: "none",
+    layout: root.verticalLayout
+  }));
+
+  // We don't want zoom-out button to appear while animating, so we hide it
+  chart.zoomOutButton.set("forceHidden", true);
+
+  // Create axes
+  // https://www.amcharts.com/docs/v5/charts/xy-chart/axes/
+  var yRenderer = am5xy.AxisRendererY["new"](root, {});
+
+  // yRenderer.grid.template.set("location", 1);
+  yRenderer.grid.template.set("forceHidden", true);
+  yRenderer.labels.template.set("forceHidden", true);
+  var yAxis = chart.yAxes.push(am5xy.CategoryAxis["new"](root, {
+    maxDeviation: 0,
+    categoryField: "product",
+    renderer: yRenderer,
+    tooltip: am5.Tooltip["new"](root, {
+      themeTags: ["axis"]
+    })
+  }));
+  var xAxis = chart.xAxes.push(am5xy.ValueAxis["new"](root, {
+    maxDeviation: 0,
+    min: 0,
+    extraMax: 0.1,
+    renderer: am5xy.AxisRendererX["new"](root, {
+      strokeOpacity: 0.1
+    })
+  }));
+
+  // Add series
+  // https://www.amcharts.com/docs/v5/charts/xy-chart/series/
+  var series = chart.series.push(am5xy.ColumnSeries["new"](root, {
+    name: "Series 1",
+    xAxis: xAxis,
+    yAxis: yAxis,
+    valueXField: "value",
+    categoryYField: "product",
+    tooltip: am5.Tooltip["new"](root, {
+      pointerOrientation: "left",
+      labelText: "{valueX}"
+    })
+  }));
+
+  // Rounded corners for columns
+  series.columns.template.setAll({
+    cornerRadiusTR: 5,
+    cornerRadiusBR: 5,
+    strokeOpacity: 0
+  });
+
+  // Make each column to be of a different color
+  series.columns.template.adapters.add("fill", function (fill, target) {
+    return chart.get("colors").getIndex(series.columns.indexOf(target));
+  });
+  series.columns.template.adapters.add("stroke", function (stroke, target) {
+    return chart.get("colors").getIndex(series.columns.indexOf(target));
+  });
+
+  // Set data
+  var data = [{
+    product: "Бады",
+    value: 2025
+  }, {
+    product: "Снижение сахара",
+    value: 1882
+  }, {
+    product: "Снеки",
+    value: 1809
+  }, {
+    product: "Корма для животных",
+    value: 1322
+  }, {
+    product: "Газированные напитки",
+    value: 1122
+  }, {
+    product: "Энергетические напитки",
+    value: 1114
+  }, {
+    product: "Безалкогольные напитки",
+    value: 984
+  }];
+  series.bullets.push(function () {
+    return am5.Bullet["new"](root, {
+      locationX: 1,
+      sprite: am5.Label["new"](root, {
+        text: "{product}",
+        fill: root.interfaceColors.get("alternativeText"),
+        centerX: am5.p100,
+        centerY: am5.p50,
+        populateText: true
+      })
+    });
+  });
+  yAxis.data.setAll(data);
+  series.data.setAll(data);
+
+  // Make stuff animate on load
+  // https://www.amcharts.com/docs/v5/concepts/animations/
+  series.appear(1000);
+  chart.appear(1000, 100);
+});
+
+/***/ }),
+
+/***/ "./src/js/components/client_portrait__donut-chart.js":
+/*!***********************************************************!*\
+  !*** ./src/js/components/client_portrait__donut-chart.js ***!
+  \***********************************************************/
+/***/ (function() {
+
+if ($('#client_portrait__donut-chart-one').length) am5.ready(function () {
+  // Create root element
+  // https://www.amcharts.com/docs/v5/getting-started/#Root_element
+  var root = am5.Root["new"]("client_portrait__donut-chart-one");
+  root._logo.dispose();
+
+  // Set themes
+  // https://www.amcharts.com/docs/v5/concepts/themes/
+  root.setThemes([am5themes_Animated["new"](root)]);
+
+  // Create chart
+  // https://www.amcharts.com/docs/v5/charts/percent-charts/pie-chart/
+  if (window.outerWidth <= 768) {
+    var chart = root.container.children.push(am5percent.PieChart["new"](root, {
+      radius: am5.percent(90),
+      innerRadius: am5.percent(40),
+      layout: root.verticalLayout
+    }));
+  } else {
+    var chart = root.container.children.push(am5percent.PieChart["new"](root, {
+      radius: am5.percent(90),
+      innerRadius: am5.percent(40),
+      layout: root.horizontalLayout
+    }));
+  }
+
+  // Create series
+  // https://www.amcharts.com/docs/v5/charts/percent-charts/pie-chart/#Series
+  var series = chart.series.push(am5percent.PieSeries["new"](root, {
+    name: "Series",
+    valueField: "value",
+    categoryField: "category",
+    alignLabels: false
+  }));
+
+  // Set data
+  // https://www.amcharts.com/docs/v5/charts/percent-charts/pie-chart/#Setting_data
+  series.data.setAll([{
+    value: 10,
+    category: "Скачивание документов"
+  }, {
+    value: 9,
+    category: "Заказы"
+  }, {
+    value: 6,
+    category: "Запрос образца"
+  }, {
+    value: 5,
+    category: "Запрос цены"
+  }, {
+    value: 4,
+    category: "Запрос документа"
+  }, {
+    value: 3,
+    category: "Просмотры карточек"
+  }, {
+    value: 3,
+    category: "Контакт в чате"
+  }]);
+
+  // Disabling ticks
+  // series.labels.template.set("visible", false);
+  series.labels.template.setAll({
+    fill: am5.color(0xffffff),
+    text: "{valuePercentTotal.formatNumber('0.00')}%[/]",
+    textType: "circular",
+    radius: 10,
+    inside: true
+  });
+  series.ticks.template.set("visible", false);
+
+  // Create legend
+  // https://www.amcharts.com/docs/v5/charts/percent-charts/legend-percent-series/
+
+  if (window.outerWidth <= 768) {
+    var legend = chart.children.push(am5.Legend["new"](root, {}));
+  } else {
+    var legend = chart.children.push(am5.Legend["new"](root, {
+      centerY: am5.percent(50),
+      y: am5.percent(50),
+      layout: root.verticalLayout
+    }));
+  }
+  legend.markerRectangles.template.setAll({
+    cornerRadiusTL: 10,
+    cornerRadiusTR: 10,
+    cornerRadiusBL: 10,
+    cornerRadiusBR: 10
+  });
+  // set value labels align to right
+  legend.valueLabels.template.setAll({
+    textAlign: "right"
+  });
+  // set width and max width of labels
+  legend.labels.template.setAll({
+    maxWidth: 200,
+    width: 200,
+    oversizedBehavior: "wrap"
+  });
+  legend.data.setAll(series.dataItems);
+
+  // Play initial series animation
+  // https://www.amcharts.com/docs/v5/concepts/animations/#Animation_of_series
+  series.appear(1000, 100);
+});
+if ($('#client_portrait__donut-chart-two').length) am5.ready(function () {
+  // Create root element
+  // https://www.amcharts.com/docs/v5/getting-started/#Root_element
+  var root = am5.Root["new"]("client_portrait__donut-chart-two");
+  root._logo.dispose();
+
+  // Set themes
+  // https://www.amcharts.com/docs/v5/concepts/themes/
+  root.setThemes([am5themes_Animated["new"](root)]);
+
+  // Create chart
+  // https://www.amcharts.com/docs/v5/charts/percent-charts/pie-chart/
+  if (window.outerWidth <= 768) {
+    var chart = root.container.children.push(am5percent.PieChart["new"](root, {
+      radius: am5.percent(90),
+      innerRadius: am5.percent(40),
+      layout: root.verticalLayout
+    }));
+  } else {
+    var chart = root.container.children.push(am5percent.PieChart["new"](root, {
+      radius: am5.percent(90),
+      innerRadius: am5.percent(40),
+      layout: root.horizontalLayout
+    }));
+  }
+
+  // Create series
+  // https://www.amcharts.com/docs/v5/charts/percent-charts/pie-chart/#Series
+  var series = chart.series.push(am5percent.PieSeries["new"](root, {
+    name: "Series",
+    valueField: "value",
+    categoryField: "category",
+    alignLabels: false
+  }));
+
+  // Set data
+  // https://www.amcharts.com/docs/v5/charts/percent-charts/pie-chart/#Setting_data
+  series.data.setAll([{
+    value: 10,
+    category: "Скачивание документов"
+  }, {
+    value: 9,
+    category: "Заказы"
+  }, {
+    value: 6,
+    category: "Запрос образца"
+  }, {
+    value: 5,
+    category: "Запрос цены"
+  }, {
+    value: 4,
+    category: "Запрос документа"
+  }, {
+    value: 3,
+    category: "Просмотры карточек"
+  }, {
+    value: 3,
+    category: "Контакт в чате"
+  }]);
+
+  // Disabling ticks
+  // series.labels.template.set("visible", false);
+  series.labels.template.setAll({
+    fill: am5.color(0xffffff),
+    text: "{valuePercentTotal.formatNumber('0.00')}%[/]",
+    textType: "circular",
+    radius: 10,
+    inside: true
+  });
+  series.ticks.template.set("visible", false);
+
+  // Create legend
+  // https://www.amcharts.com/docs/v5/charts/percent-charts/legend-percent-series/
+
+  if (window.outerWidth <= 768) {
+    var legend = chart.children.push(am5.Legend["new"](root, {}));
+  } else {
+    var legend = chart.children.push(am5.Legend["new"](root, {
+      centerY: am5.percent(50),
+      y: am5.percent(50),
+      layout: root.verticalLayout
+    }));
+  }
+  legend.markerRectangles.template.setAll({
+    cornerRadiusTL: 10,
+    cornerRadiusTR: 10,
+    cornerRadiusBL: 10,
+    cornerRadiusBR: 10
+  });
+  // set value labels align to right
+  legend.valueLabels.template.setAll({
+    textAlign: "right"
+  });
+  // set width and max width of labels
+  legend.labels.template.setAll({
+    maxWidth: 200,
+    width: 200,
+    oversizedBehavior: "wrap"
+  });
+  legend.data.setAll(series.dataItems);
+
+  // Play initial series animation
+  // https://www.amcharts.com/docs/v5/concepts/animations/#Animation_of_series
+  series.appear(1000, 100);
+});
+
+/***/ }),
+
 /***/ "./src/js/components/displaying.js":
 /*!*****************************************!*\
   !*** ./src/js/components/displaying.js ***!
@@ -37877,6 +38352,12 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _components_actions_by_category__WEBPACK_IMPORTED_MODULE_54___default = /*#__PURE__*/__webpack_require__.n(_components_actions_by_category__WEBPACK_IMPORTED_MODULE_54__);
 /* harmony import */ var _components_action_in_store__WEBPACK_IMPORTED_MODULE_55__ = __webpack_require__(/*! ./components/action-in-store */ "./src/js/components/action-in-store.js");
 /* harmony import */ var _components_action_in_store__WEBPACK_IMPORTED_MODULE_55___default = /*#__PURE__*/__webpack_require__.n(_components_action_in_store__WEBPACK_IMPORTED_MODULE_55__);
+/* harmony import */ var _components_client_portrait_donut_chart__WEBPACK_IMPORTED_MODULE_56__ = __webpack_require__(/*! ./components/client_portrait__donut-chart */ "./src/js/components/client_portrait__donut-chart.js");
+/* harmony import */ var _components_client_portrait_donut_chart__WEBPACK_IMPORTED_MODULE_56___default = /*#__PURE__*/__webpack_require__.n(_components_client_portrait_donut_chart__WEBPACK_IMPORTED_MODULE_56__);
+/* harmony import */ var _components_client_portrait_column__WEBPACK_IMPORTED_MODULE_57__ = __webpack_require__(/*! ./components/client_portrait__column */ "./src/js/components/client_portrait__column.js");
+/* harmony import */ var _components_client_portrait_column__WEBPACK_IMPORTED_MODULE_57___default = /*#__PURE__*/__webpack_require__.n(_components_client_portrait_column__WEBPACK_IMPORTED_MODULE_57__);
+
+
 
 
 
